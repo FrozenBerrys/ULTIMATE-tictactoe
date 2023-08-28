66c0d393ae894fbceca3ba8814f76ac9ecb3c350
Boko <bokosandesu@gmail.com>
2023-08-28 20:23:05 +0800

    Added Rule 1, Added Lose/Win condition/game restart

--- main.py
+++ main.py
@@ -67,18 +67,21 @@ def valid(pos, grid, biggrid):
     coord = getSquare(pos)
     bigx = coord[0] // 3
     bigy = coord[1] // 3
-    print(bigx,bigy,coord[0]%3,coord[1]%3)
     if biggrid[bigx][bigy] != 0: #array cartesian discrepancy
-        print(biggrid)
-        print("BIG")
         return False
     if grid[bigx][bigy][coord[0] % 3][coord[1] % 3] != 0:
-        print("NO")
         return False
-    print("VALID",bigx,bigy,coord[0]%3,coord[1]%3)
+    return True
+
+def rule(pos, last):
+    coord = getSquare(pos)
+    bigx = coord[0] // 3
+    bigy = coord[1] // 3
+    if last != [-1,-1]:
+        if last[0] != bigx or last[1] != bigy:
+            return False
     return True
     
-
 def tictactoe(grid):
     if (grid[0][0] == -1 and grid[0][1] == -1 and grid[0][2] == -1) or (grid[1][0] == -1 and grid[1][1] == -1 and grid[1][2] == -1) or (grid[2][0] == -1 and grid[2][1] == -1 and grid[2][2] == -1):
         return -1
@@ -110,6 +113,8 @@ while Game:
             [[[0 , 0 , 0], [0 , 0 , 0], [0 , 0 , 0]],[[0 , 0 , 0], [0 , 0 , 0], [0 , 0 , 0]],[[0 , 0 , 0], [0 , 0 , 0], [0 , 0 , 0]]]
         ]
         turn = 1
+        last = [-1,-1]
+        all = 1
         initialize = 0
 
     for event in pygame.event.get():
@@ -117,15 +122,25 @@ while Game:
             pygame.quit()
         mouse = pygame.mouse.get_pos()
         pos = getSquare(mouse)
-        if event.type == pygame.MOUSEBUTTONDOWN and valid(mouse, grid, biggrid) and getSquare(mouse) != (-1,-1):
+        if event.type == pygame.MOUSEBUTTONDOWN and valid(mouse, grid, biggrid) and rule(mouse, last) and getSquare(mouse) != (-1,-1):
             grid[pos[0] // 3][pos[1] // 3][pos[0] % 3][pos[1] % 3] = turn
+            last[0] = pos[0] % 3; last[1] = pos[1] % 3
             turn = 0 - turn
+
+        elif event.type == pygame.MOUSEBUTTONDOWN and tictactoe(grid[last[0]][last[1]]) != 0:
+            grid[pos[0] // 3][pos[1] // 3][pos[0] % 3][pos[1] % 3] = turn
+            last[0] = pos[0] % 3; last[1] = pos[1] % 3
+            turn = 0 - turn
+
+    if tictactoe(grid[last[0]][last[1]]) != 0:
+        all = 1
+    else: all = 0
 
     for i in range(3):
         for j in range(3):
             if tictactoe(grid[i][j]) != 0:
                 biggrid[i][j] = tictactoe(grid[i][j])
-    
+
     #####################################################################################
     ##################################################################################### DISPLAY
     screen.blit(game_surf, game_rect)
@@ -136,7 +151,7 @@ while Game:
         pygame.draw.line(game_surf, "Black", (i[0]+6,i[1]+54), (i[0]+166,i[1]+54), 4)
         pygame.draw.line(game_surf, "Black", (i[0]+6,i[1]+114), (i[0]+166,i[1]+114), 4)
     ##################################################################################### fancy selected highlight
-    if valid(mouse, grid, biggrid):
+    if valid(mouse, grid, biggrid) and all == 1 or valid(mouse, grid, biggrid) and rule(mouse, last) :
         ex = ((pos[0] // 3) * 180) + ((pos[0] % 3) * 60)
         why = ((pos[1] // 3) * 180) + ((pos[1] % 3) * 60)
         highlight = pygame.Surface((50,50))
@@ -146,7 +161,7 @@ while Game:
             highlight.fill("Blue")
         highlight.set_alpha(150)
         screen.blit(highlight, (why,ex))
-    #####################################################################################
+    ##################################################################################### NORMAL O AND X'S
     for n in range(3):
         for i in range(3):
             for j in range(3):
@@ -159,7 +174,15 @@ while Game:
                         pygame.draw.line(screen, "Red", (x+48, y), (x, y+48), 8)
                     if grid[n][i][j][k] == -1:
                         pygame.draw.circle(screen, "Blue", (x+24, y+24), 24, 8)
-    #####################################################################################             
+    #####################################################################################  INVALID BIG GRIDS
+    for i in range(3):
+        for j in range(3):
+            if (i != last[0] or j != last[1]) and last[0] != -1 and all == 0:
+                invalid = pygame.Surface((180,180)) 
+                invalid.fill("Black")
+                invalid.set_alpha(90)
+                screen.blit(invalid, (j*180, i*180))
+    #####################################################################################  BIG O AND X'S        
     for i in range(3):
         for j in range(3):
             if biggrid[i][j] == 1:
@@ -167,7 +190,28 @@ while Game:
                 pygame.draw.line(screen, "Red", (j*180+170, i*180), (j*180,i*180+170), 16)
             if biggrid[i][j] == -1:
                 pygame.draw.circle(screen, "Blue", (j*180+85, i*180+85), 85, 16)
+    #####################################################################################
+    ### LOSE/WIN CONDITION
+    if tictactoe(biggrid) == 1:
+        redwins = pygame.Surface((520,520))
+        redwins.fill("Red")
+        redwins.set_alpha(60)
+        screen.blit(redwins, (10,10))
+        pygame.draw.line(screen, "Red", (10,10), (530,530), 24)
+        pygame.draw.line(screen, "Red", (530,10), (10,530), 24)
+        pygame.display.flip()
+        pygame.time.wait(5000)
+        initialize = 1
 
-                        
+    if tictactoe(biggrid) == -1:
+        bluewins = pygame.Surface((520,520))
+        bluewins.fill("Blue")     
+        bluewins.set_alpha(60)
+        screen.blit(bluewins, (10,10))
+        pygame.draw.circle(screen, "Blue", (270,270), 260, 24)
+        pygame.display.flip()
+        pygame.time.wait(5000)
+        initialize = 1
+
     pygame.display.flip()
     clock.tick(60)
